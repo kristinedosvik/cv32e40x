4fc7a6e410b7cdb5ec1fe0f82f8a4e5ff4a57b60
Kristine Dosvik <Kristine.Dosvik@silabs.com>
2021-07-22 02:43:11 -0500

    test commit for new branch

--- rtl/cv32e40x_alu.sv
+++ rtl/cv32e40x_alu.sv
@@ -7,7 +7,6 @@
 // this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
-
 /*
  // New shifter based on https://github.com/riscv/riscv-bitmanip/blob/main-history/verilog/rvb_shifter/rvb_shifter.v
 
